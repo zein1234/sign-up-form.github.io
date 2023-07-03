6f4320129bee0c67781ea2fb6dc1b8403045f253
zain srio <zein.srio@gmail.com>
2023-07-03 23:28:52 +0200

    assigned variables

---
+++ index.js
@@ -0,0 +1,10 @@
+// Get form elements
+const formContainer = document.getElementById('form-container');
+const emailForm = document.getElementById('email-form');
+const successMessage = document.getElementById('success-message');
+const backButton = document.getElementById('back-button');
+const errorMessage = document.getElementById('error-message');
+const emailInput = document.getElementById('email-input');
+
+const receivedEmailValue = document.getElementById('received-email');
+
