90cf179628019b5ca484d8c6df245dfac9f6ea21
zain srio <zein.srio@gmail.com>
2023-07-04 11:48:10 +0200

    added some changes to css

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ const emailInput = document.getElementById('email-input');
 
 const receivedEmailValue = document.getElementById('received-email');
 
+
+// Show success message and hide form
+emailForm.addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent form submission
+  if (isValidEmail(emailInput.value)) {
+    const enteredEmail = emailInput.value;
+    successMessage.classList.remove('hide');
+    formContainer.classList.add('hide');
+    errorMessage.classList.add('hide');
+
+    receivedEmailValue.textContent = enteredEmail;
+
+  } else {
+    errorMessage.classList.remove('hide');
+    emailInput.style.backgroundColor = "rgb(253, 200, 200)";
+    emailInput.style.border = "1px solid rgb(255,92,92)";
+  }
+});
+
+// Revert back to form when the back button is clicked
+backButton.addEventListener('click', function () {
+  successMessage.classList.add('hide');
+  formContainer.classList.remove('hide');
+});
+
+// Validate email address using a regular expression
+function isValidEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
